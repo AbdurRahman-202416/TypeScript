b6cd1bf4cb9151a887c47a079b56be55f30e8c10
Abdur Rahman <abdurrahman19011@gmail.com>
2025-01-25 17:23:14 +0600

    add

---
+++ FIrstProgram/Index.js
@@ -0,0 +1,13 @@
+//string
+var username = 'John Doe';
+console.log(username);
+//number
+var age = 30;
+console.log(age);
+//boolean
+var isStudent = true;
+console.log(isStudent);
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(1, 2));
---
+++ FIrstProgram/Index.ts
@@ -0,0 +1,15 @@
+//string
+let username: string = 'John Doe'
+console.log(username)
+//number
+let age: number = 30
+console.log(age)
+
+//boolean
+let isStudent: boolean = true
+console.log(isStudent)
+
+function sum (num1: number, num2: number): number {
+  return num1 + num2
+}
+console.log(sum(1, 2))
