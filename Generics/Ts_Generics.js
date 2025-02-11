5e19ebda23905efe919882cc25a00caf3888619e
Shanto <abdurrahman19011@gmail.com>
2025-02-11 22:09:33 +0600

    new topic

--- Generics/Ts_Generics.js
+++ Generics/Ts_Generics.js
@@ -1,1 +1,28 @@
 "use strict";
+// function age1(age: string): string {
+//   return age;
+// }
+// function age2(age: number): number {
+//   return age;
+// }
+// function age3(age:boolean): boolean {
+//   return age;
+// }
+//console.log(age1("20"));
+//console.log(age2(20));
+//console.log(age3(true));
+// age3(11); // Error: Argument of type '11' is not assignable to parameter of type 'boolean'.
+function parsonAge(age) {
+    return age;
+}
+parsonAge("20");
+parsonAge(20);
+parsonAge(true);
+// Generic with multiple objects
+function combineObject(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+combineObject({ name: "John" }, { age: 20 });
+combineObject({ name: "John" }, "20"); // Error: Argument of type '20' is not assignable to parameter of type 'object'.
+console.log(combineObject({ name: "John" }, { age: 20 }));
+//  {name: "John", age: 20}
---
+++ Generics/Ts_Generics.ts
@@ -0,0 +1,36 @@
+// function age1(age: string): string {
+//   return age;
+// }
+// function age2(age: number): number {
+//   return age;
+// }
+// function age3(age:boolean): boolean {
+//   return age;
+// }
+//console.log(age1("20"));
+//console.log(age2(20));
+//console.log(age3(true));
+
+// age3(11); // Error: Argument of type '11' is not assignable to parameter of type 'boolean'.
+
+function parsonAge<daynamicType>(age: daynamicType): daynamicType {
+  return age;
+}
+
+parsonAge<string>("20");
+parsonAge<number>(20);
+parsonAge<boolean>(true);
+
+// Generic with multiple objects
+function combineObject<T extends object, U extends object>(
+  obj1: T,
+  obj2: U
+): T & U {
+  return { ...obj1, ...obj2 };
+}
+combineObject({ name: "John" }, { age: 20 });
+
+combineObject({ name: "John" }, "20"); // Error: Argument of type '20' is not assignable to parameter of type 'object'.
+
+console.log(combineObject({ name: "John" }, { age: 20 }));
+//  {name: "John", age: 20}
